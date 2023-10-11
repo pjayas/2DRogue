63b7a0ec85b19950d7f907715fe37908fc85cb5e
pjayas <115007528+pjayas@users.noreply.github.com>
2023-10-12 00:24:36 +0900

    Dash

--- 2DRogue/Assets/Scripts/PlayerMovement.cs
+++ 2DRogue/Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Vector2 targetPos;
+    public float dashRange;
     public float speed;
     private Vector2 direction;
     private Animator animator;
+    private enum Facing  {UP,DOWN, LEFT, RIGHT};
+    private Facing FacingDir = Facing.LEFT;
 
     void Start()
     {
@@ -40,18 +44,46 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             direction += Vector2.up;
+            FacingDir = Facing.UP;
         }
         if (Input.GetKey(KeyCode.A))
         {
             direction += Vector2.left;
+            FacingDir = Facing.LEFT;
         }
         if (Input.GetKey(KeyCode.S))
         {
             direction += Vector2.down;
+            FacingDir = Facing.DOWN;
         }
         if (Input.GetKey(KeyCode.D))
         {
             direction += Vector2.right;
+            FacingDir = Facing.RIGHT;
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            Vector2 currentPos= transform.position;
+            targetPos = Vector2.zero;
+            if (FacingDir == Facing.UP)
+            {
+                targetPos.y = 1;
+            }
+            else if (FacingDir == Facing.DOWN)
+            {
+                targetPos.y = -1;
+            }
+            else if (FacingDir == Facing.LEFT)
+            {
+                targetPos.x = - 1;
+            }
+            else if (FacingDir == Facing.RIGHT)
+            {
+                targetPos.x = 1;
+            }
+            transform.Translate(targetPos * dashRange);
+
         }
     }
 
